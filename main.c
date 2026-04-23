a1271dad25f61ec70baa25135ee10b2cf54932fb
Miguel da Silva Santos <miguelzin9990@gmail.com>
2026-04-22 22:39:12 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int i, soma = 2;
+
+    for (i = 1; i < 7; i++) {
+        soma += (4 + 1);
+    }
+
+    printf("A soma das pecas com 4 e: %d\n", soma);
+
+    system("pause");
+    return 0;
+}
